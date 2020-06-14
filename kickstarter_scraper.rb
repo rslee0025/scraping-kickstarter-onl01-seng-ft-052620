21c89338cbb36b0f13df3cff2ebd062829347004
Rachel S. Lee <rslee0025@gmail.com>
2020-06-14 19:08:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,4 +2,9 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
-  html = File.read
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  
+  
