f13fe6043183b4c6143f3366869c329115de8f6b
Rachel S. Lee <rslee0025@gmail.com>
2020-06-14 19:03:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,8 +10,8 @@ require 'pry'
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash	
-  	  html = File.read('fixtures/kickstarter.html')
-end 	  kickstarter = Nokogiri::HTML(html)
+  html = File.read('fixtures/kickstarter.html') kickstarter = Nokogiri::HTML(html)
+end
 
   projects = {}
 
